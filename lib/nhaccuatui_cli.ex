1213fbf132352b5822735196e7a3fa4f40c18fe8
Long Nguyen <long.polyglot@gmail.com>
2017-05-29 15:25:04 +0700

    Implement search functionality

--- lib/nhaccuatui_cli.ex
+++ lib/nhaccuatui_cli.ex
@@ -2,6 +2,7 @@ defmodule NhaccuatuiCli do
   @topnhacviet "http://www.nhaccuatui.com/ajax/get-top-20?type=&genre=nhac-viet&isHome=true"
   @topnhacus "http://www.nhaccuatui.com/ajax/get-top-20?type=&genre=au-my&isHome=true"
   @topnhachan "http://www.nhaccuatui.com/ajax/get-top-20?type=&genre=nhac-han&isHome=true"
+  @search_url "http://www.nhaccuatui.com/ajax/search?"
 
   def main(args) do
     options = parse_args(args)
@@ -9,7 +10,23 @@ defmodule NhaccuatuiCli do
       "top-vn" -> get_top(@topnhacviet, "Top 10 nhạc Việt")
       "top-us" -> get_top(@topnhacus, "Top 10 nhạc Âu Mỹ")
       "top-kr" -> get_top(@topnhachan, "Top 10 nhạc Hàn")
+      "search" -> search(Keyword.get(options, :query))
       _ -> get_top(@topnhacviet, "Top 10 nhạc Việt")
+    end
+  end
+
+  def search(query) do
+    url = "#{@search_url}q=#{query}"
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        temp = body |> Poison.Parser.parse!
+
+        print_entities(temp["data"]["song"], "songs")
+        print_entities(temp["data"]["video"], "videos")
+        print_entities(temp["data"]["singer"], "singers")
+        print_entities(temp["data"]["playlist"], "playlists")
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
     end
   end
 
@@ -36,5 +53,18 @@ defmodule NhaccuatuiCli do
       switches: [action: :string]
     )
     options
+  end
+
+  defp print_entities(entities, type) do
+    IO.puts "*** Search result by #{type} ***"
+    Enum.each(entities, fn (song) -> print_entity(song) end)
+    IO.puts "*************************"
+    IO.puts "\n"
+  end
+
+  defp print_entity(entity) do
+    name = entity["name"]
+    url = entity["url"]
+    IO.puts "#{name} - #{url}"
   end
 end
